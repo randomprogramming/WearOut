01aba2c0993d7c23b350578fd6755b10e0077de1
randomprogramming <isnipedodac@gmail.com>
2020-03-15 12:02:14 +0100

    added icons to the project and added search icon to searchbar

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import {View, TextInput, StyleSheet} from 'react-native';
+import Icon from 'react-native-vector-icons/dist/Feather';
+
 import CONSTANTS from '../global_state/constants';
 
 const SearchBar = () => {
   return (
     <View style={styles.mainContainer}>
-      <TextInput
-        placeholder="Search streetwear or people..."
-        placeholderTextColor={CONSTANTS.TEXT_COLOR}
-        selectionColor={CONSTANTS.ACCENT_COLOR}
-        style={styles.input}
-      />
+      <View style={styles.iconContainer}>
+        <Icon name="search" size={32} color={CONSTANTS.ACCENT_COLOR} />
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Search streetwear or people..."
+          placeholderTextColor={CONSTANTS.TEXT_COLOR}
+          selectionColor={CONSTANTS.ACCENT_COLOR}
+          style={styles.input}
+        />
+      </View>
     </View>
   );
 };
@@ -20,6 +27,15 @@ const styles = StyleSheet.create({
     marginVertical: 15,
     marginHorizontal: 25,
     backgroundColor: CONSTANTS.VERY_LIGHT_BACKGROUND,
+    flexDirection: 'row',
+  },
+  iconContainer: {
+    flex: 0,
+    justifyContent: 'center',
+    marginLeft: 4,
+  },
+  inputContainer: {
+    flex: 1,
   },
   input: {
     color: CONSTANTS.TEXT_COLOR,
