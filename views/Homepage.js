edcad0bd8ae51d60937dfe7d06e701ecac270c3d
randomprogramming <isnipedodac@gmail.com>
2020-03-15 12:47:09 +0100

    added a delete button on searchbar

--- views/Homepage.js
+++ views/Homepage.js
@@ -1,18 +1,41 @@
-import React from 'react';
-import {StyleSheet, View, Text, ScrollView} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, View, Text, ScrollView, StatusBar} from 'react-native';
 import LogoHeader from '../components/LogoHeader';
 import SearchBar from '../components/SearchBar';
 import CONSTANTS from '../global_state/constants';
 
 export const Homepage = () => {
+  const [searchValue, setsearchValue] = useState('');
+
+  const handleInput = newInput => {
+    setsearchValue(newInput);
+  };
+
+  const deleteHandler = () => {
+    setsearchValue('');
+  };
+
   return (
     <View style={styles.mainContainer}>
+      <StatusBar backgroundColor={CONSTANTS.BLACK_BACKGROUND} />
       <View style={styles.headerContainer}>
         <LogoHeader />
       </View>
       <View style={styles.contentContainer}>
-        <SearchBar />
-        <Text>Content Text</Text>
+        <View>
+          <SearchBar
+            inputHandler={handleInput}
+            value={searchValue}
+            deleteHandler={deleteHandler}
+          />
+        </View>
+        <View>
+          {searchValue.length === 0 ? (
+            <Text style={{color: 'white'}}>Content Text</Text>
+          ) : (
+            <Text style={{color: 'white'}}>search Text</Text>
+          )}
+        </View>
       </View>
     </View>
   );
