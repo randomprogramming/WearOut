851a9420b571d3f2770e6d671f96bf4fbe538c4a
randomprogramming <isnipedodac@gmail.com>
2020-03-17 16:28:49 +0100

    implemented screen tabs and ability to change between them

--- App.js
+++ App.js
@@ -1,19 +1,57 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import AntDesignIcon from 'react-native-vector-icons/AntDesign';
 
 import { COLORS, SCREEN_NAMES } from './global_state/constants';
 import Homescreen from './views/Homescreen';
 import Search from './views/Search';
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
-    <Stack.Navigator initialRouteName={SCREEN_NAMES.HOME} headerMode="none">
-      <Stack.Screen name={SCREEN_NAMES.HOME} component={Homescreen} />
-      <Stack.Screen name={SCREEN_NAMES.SEARCH} component={Search} />
-    </Stack.Navigator>
+    <Tab.Navigator
+      initialRouteName={SCREEN_NAMES.HOME}
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          // TODO: Rewrite this so that we can add new tabs in only one place, instead of adding a icon and a tab.screen
+          //Depending on the tab name, return a icon to match that tab and show it in the bottom bar
+          switch (route.name) {
+            case SCREEN_NAMES.HOME:
+              //If the screen is home
+              return (
+                <AntDesignIcon
+                  name="home"
+                  size={size}
+                  color={focused ? COLORS.ACCENT_COLOR : COLORS.TEXT_COLOR}
+                />
+              );
+            case SCREEN_NAMES.SEARCH:
+              //If the screen is search
+              return (
+                <AntDesignIcon
+                  name="search1"
+                  size={size}
+                  color={focused ? COLORS.ACCENT_COLOR : COLORS.TEXT_COLOR}
+                />
+              );
+            default:
+              //If the screen is something else, this shouldn't happen
+              return (
+                <AntDesignIcon
+                  name="closecircleo"
+                  size={size}
+                  color={focused ? COLORS.ACCENT_COLOR : COLORS.TEXT_COLOR}
+                />
+              );
+          }
+        },
+      })}
+      tabBarOptions={{ showLabel: false }}>
+      <Tab.Screen name={SCREEN_NAMES.HOME} component={Homescreen} />
+      <Tab.Screen name={SCREEN_NAMES.SEARCH} component={Search} />
+    </Tab.Navigator>
   );
 };
 
